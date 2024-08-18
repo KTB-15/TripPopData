43d5346a7c65d1d09933578e3a866688018b2a6f
Sin Han <vrdhan212@gmail.com>
2024-08-18 19:54:59 +0900

    update: save all sgg

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,13 @@
 from common.logger import get_logger
 from common.metadata import CSV_NAME
-from database.load import DataSaver
+from database.save import DataSaver
 from database.model import SGG
+from database.format import to_SGG
 import os
 import pandas as pd
 
 logger = get_logger('MAIN')
 
-logger.info(os.getcwd())
 data = pd.read_csv(CSV_NAME.sgg)
 df = data.iloc[0]
 logger.info(df.keys())
@@ -18,6 +18,4 @@ SGG_data_saver = DataSaver(
 )
 
 SGG_data_saver.load_csv()
-sgg_df = SGG_data_saver.data.iloc[0]
-
-logger.info(sgg_df)
+SGG_data_saver.save_all(to_SGG)
