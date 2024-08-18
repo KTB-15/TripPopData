35caf3605add6002edaa0285bbb68cd216d05523
Sin Han <vrdhan212@gmail.com>
2024-08-18 20:53:34 +0900

    update: save all member

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,8 @@
 from common.logger import get_logger
 from common.metadata import CSV_NAME
 from database.save import DataSaver
-from database.model import SGG
-from database.format import to_SGG
+from database.model import SGG, Member
+from database.format import to_SGG, to_member
 import os
 import pandas as pd
 
@@ -16,6 +16,13 @@ SGG_data_saver = DataSaver(
     file_name=CSV_NAME.sgg,
     model_class=SGG,
 )
+member_data_saver = DataSaver(
+    file_name=CSV_NAME.traveler,
+    model_class=Member
+)
 
-SGG_data_saver.load_csv()
-SGG_data_saver.save_all(to_SGG)
+# SGG_data_saver.load_csv()
+# SGG_data_saver.save_all(to_SGG)
+
+member_data_saver.load_csv()
+member_data_saver.save_all(to_member)
