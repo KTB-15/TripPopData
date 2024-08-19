5d7077b42d583fdf15d3ce68890e207cab036889
Sin Han <vrdhan212@gmail.com>
2024-08-19 17:45:54 +0900

    fix: close session in all case

--- database/conn.py
+++ database/conn.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from typing import Union, Any, Generic, TypeVar
+from contextlib import contextmanager
 
 from common.logger import get_logger
 
@@ -14,6 +15,19 @@ Base = declarative_base()
 Base.metadata.create_all(_engine)
 
 Session = sessionmaker(bind=_engine)
+
+
+# @contextmanager
+# def session_scope():
+#     session = Session()
+#     try:
+#         yield session
+#         session.commit()
+#     except:
+#         session.rollback()
+#         raise
+#     finally:
+#         session.close()
 
 
 def insert(data: Base) -> bool:
@@ -28,16 +42,17 @@ def insert(data: Base) -> bool:
         session.close()
         return True
     except IntegrityError as e:
-        LOGGER.error(f"무결성 위반됨: {str(e)}")
+        # LOGGER.error(f"무결성 위반됨: {str(e)}")
         session.rollback()
         session.close()
         return True
     except SQLAlchemyError as e:
         LOGGER.error(f"SQL 에러: {str(e)}")
+        session.rollback()
     except Exception as e:
         LOGGER.error(f"DB 로드 실패: {str(e)}")
-    finally:
         session.rollback()
+    finally:
         session.close()
     return False
 
@@ -53,12 +68,17 @@ def get_filter(field: str, model_class: T, expected: Any) -> Union[T | None]:
         return None
     try:
         _field = getattr(model_class, field)
+        result = session.query(model_class).filter(_field == expected).first()
+        session.close()
+        return result
     except AttributeError:
         LOGGER.error(f"{model_class.__name__}에 {field}가 없어요.")
         session.close()
         return None
-    session.close()
-    return session.query(model_class).filter(_field == expected).first()
+    except Exception as e:
+        LOGGER.error(f"SQL ERROR: {str(e)}")
+        session.close()
+        return None
 
 
 __all__ = [Base, Session, insert, get_filter]
