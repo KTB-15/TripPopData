2534d5e244b12791f49f3be979a3a423d9d16950
Sin Han <vrdhan212@gmail.com>
2024-08-19 13:55:43 +0900

    add: manage all csv folders

--- common/metadata.py
+++ common/metadata.py
@@ -8,6 +8,10 @@ def _to_file_name(name: str) -> str:
     return _PREFIX + name + _POSTFIX
 
 
+def _to_file_name_with_folder(folder: str, file: str) -> str:
+    return _PREFIX + folder + file + _POSTFIX
+
+
 @dataclass
 class _CSVName:
     codea: str
@@ -17,6 +21,14 @@ class _CSVName:
     travel: str
     traveler: str
     visit_area_info: str
+
+
+@dataclass
+class _CSVFolder:
+    a: _CSVName
+    b: _CSVName
+    c: _CSVName
+    d: _CSVName
 
 
 CSV_NAME = _CSVName(
@@ -29,4 +41,20 @@ CSV_NAME = _CSVName(
     visit_area_info=_to_file_name('visit_area_info')
 )
 
-__all__ = [CSV_NAME]
+
+def get_csv_name(folder: str):
+    _folder = folder + '/'
+    return _CSVName(
+        traveler=_to_file_name_with_folder(_folder, 'traveler'),
+        visit_area_info=_to_file_name_with_folder(_folder, 'visit_area_info'),
+    )
+
+
+CSV_NAME_V2 = _CSVFolder(
+    a=get_csv_name('a'),
+    b=get_csv_name('b'),
+    c=get_csv_name('c'),
+    d=get_csv_name('d'),
+)
+
+__all__ = [CSV_NAME, CSV_NAME_V2]
